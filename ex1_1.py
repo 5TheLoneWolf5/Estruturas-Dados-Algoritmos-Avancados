e8fbe5dd120837013036a29230f5a17bbc01deaf
5TheLoneWolf5 <89516641+5TheLoneWolf5@users.noreply.github.com>
2025-02-13 20:46:20 -0300

    Initial commit.

---
+++ ex1_1.py
@@ -0,0 +1,7 @@
+"""
+
+As estruturas de dado heap são úteis ao aplicá-las em situações que requerem um tipo de prioridade definida pela regra de negócio.
+
+Alguns exemplos são: fila de atendimento de um hospital, pedidos de um restaurante,
+
+"""
